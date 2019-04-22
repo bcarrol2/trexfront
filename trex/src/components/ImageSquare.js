d837335ddb3c19cf9bd062f7bd7b6de1e0768dea
Blair <blairacarroll@gmail.com>
2019-04-22 16:44:10 -0400

    quote block div and component setup

--- trex/src/components/ImageSquare.js
+++ trex/src/components/ImageSquare.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import QuoteSquare from './QuoteSquare';
 
 class ImageSquare extends React.Component {
     render(){
         return(
             <div className="square">
                 <h1>Hello world</h1>
+                <button>Generate New Picture</button>
+                <div>
+                    <img id="imgsum" src="../timage1.jpg" />
+                </div>
+                <QuoteSquare />
             </div>
         )
     }
---
+++ trex/src/components/QuoteSquare.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class QuoteSquare extends React.Component {
+    render(){
+        return(
+            <div id="quote_sum">
+                <h1>hi</h1>
+            </div>
+        )
+    }
+}
+
+export default QuoteSquare;
