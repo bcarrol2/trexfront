fb68716c4dbe2e59aa7dd201b000a41cb180d991
Blair <blairacarroll@gmail.com>
2019-04-23 19:00:25 -0400

    deleted useless component

--- trex/src/App.js
+++ trex/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Quote from './components/Quote';
 import ImageSquare from './components/ImageSquare';
-import logo from './logo.svg';
 import './main.css';
 import './App.css';
 
@@ -9,7 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Quote /> */}
         <ImageSquare />
       </div>
     );
